5b25ee16bc1b1fe39cabf58fc77bd5553f679516
Erika.K <koguchi.e915@gmail.com>
2024-11-05 14:10:50 +0000

    [Add]7-3 削除機能3

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -42,15 +42,11 @@ class ListsController < ApplicationController
 
   # 削除機能
   def destroy
-    # データ（レコード）を1件取得(変数listに代入)
-    list = List.find(params[:id])
-    # データ（レコード）を削除（代入したものを削除）
-    list.destroy
-    # 投稿一覧画面へリダイレクト  
-    redirect_to "/lists"
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト  
   end
 
-  
   # ストロングパラメータ
   private
   def list_params
